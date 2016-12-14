45e12daea5aa1ab9aa0729928b31aa711d4bc71a
Kuroko <fairyland0902@gmail.com>
2016-12-14 22:51:50 +0800

    the repo is a name-changed repo of the previous project NeedForSpeed

---
+++ README.md
@@ -0,0 +1,14 @@
+# BlockyRoads
+
+This is a project of **Digtial Logic Design** in ZJU. This project is designed for *Nexys 4* FPGA. In this project, we'd like to achieve:
+
+1. Interaction with PS/2 keyboard and VGA monitor.
+2. Physical simulation of collide between cars and obstacles.
+3. Other features to make this game more interesting.
+
+
+
+# Features planned to add
+
+1. VRAM(Double Buffer)
+2. MVC
---
+++ resource/coe/bmp2coe/bmp2coe/bmp2coe.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <windows.h>
+
+int loadBitmap(const char *fileIn, const char *fileOut)
+{
+	FILE *fp;
+	BITMAPFILEHEADER bitmapFileHeader;
+	BITMAPINFOHEADER bitmapInfoHeader;
+	char *data;
+	fp = fopen(fileIn, "rb");
+	if (!fp) return -1;
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+	if (bitmapFileHeader.bfType != 0x4d42) return -2;
+	fread(&bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+	fseek(fp, bitmapFileHeader.bfOffBits, SEEK_SET);
+	data = (char*)malloc(bitmapInfoHeader.biSizeImage);
+	if (!data) return -3;
+	fread(data, 1, bitmapInfoHeader.biSizeImage, fp);
+	fclose(fp);
+
+	fp = fopen(fileOut, "w");
+	if (!fp) return -4;
+	int i, r, g, b, color;
+	for (i = 0; i < bitmapInfoHeader.biSizeImage; i += 3)
+	{
+		r = data[i + 2];
+		g = data[i + 1];
+		b = data[i];
+		r &= 0xff;
+		g &= 0xff;
+		b &= 0xff;
+		color = ((b << 4) & 0xf00) | (g & 0xf0) | ((r >> 4) & 0xf);
+		if (i % 48 == 0)
+			fprintf(fp, "\n\t.half\t0x%04x", color);
+		else
+			fprintf(fp, ", 0x%04x", color);
+	}
+	fclose(fp);
+
+	free(data);
+	return 0;
+}
+int loadBitmapBW(const char *fileIn, const char *fileOut) // 0 for black 1 for white
+{
+	FILE *fp;
+	BITMAPFILEHEADER bitmapFileHeader;
+	BITMAPINFOHEADER bitmapInfoHeader;
+	char *data;
+	fp = fopen(fileIn, "rb");
+	if (!fp) return -1;
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+	if (bitmapFileHeader.bfType != 0x4d42) return -2;
+	fread(&bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+	fseek(fp, bitmapFileHeader.bfOffBits, SEEK_SET);
+	data = (char*)malloc(bitmapInfoHeader.biSizeImage);
+	if (!data) return -3;
+	//printf("%s", data); // use for debug
+	fread(data, 1, bitmapInfoHeader.biSizeImage, fp);
+	fclose(fp);
+
+	fp = fopen(fileOut, "w");
+	if (!fp) return -4;
+	int i, r, g, b, color;
+	unsigned int d = 0;
+	fprintf(fp, "memory_initialization_radix=16;\nmemory_initialization_vector = \n");
+	for (i = 0; i < bitmapInfoHeader.biSizeImage; i += 3)
+	{
+		r = data[i + 2];
+		g = data[i + 1];
+		b = data[i];
+		r &= 0xff;
+		g &= 0xff;
+		b &= 0xff;
+		color = ((b << 4) & 0xf00) | (g & 0xf0) | ((r >> 4) & 0xf);
+		fprintf(fp, "%04x", color);
+		if (i + 3 < bitmapInfoHeader.biSizeImage)
+			fprintf(fp, ",\n");
+	}
+	fprintf(fp, ";");
+	fclose(fp);
+	free(data);
+	return 0;
+}
+
+
+int main(void)
+{
+	loadBitmapBW("123.bmp", "background.coe");
+	
+	return 0;
+}
